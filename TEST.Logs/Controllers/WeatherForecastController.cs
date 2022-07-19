a3002eaaca0f3252531202bcca6f293752d9d611
Juan Diego Morales del Corral <dmdelcor@gmail.com>
2022-07-19 07:16:43 -0500

    Added logging functionality

---
+++ TEST.Logs/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TEST.Logs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets a random weather forecast to create log entries.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                _logger.Log(LogLevel.Information, $"Hit: {nameof(WeatherForecastController)}.{nameof(Get)}");
+                var rng = new Random();
+                var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+                _logger.Log(LogLevel.Information, $"{JsonSerializer.Serialize(result)}");
+                return result;
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, $"{JsonSerializer.Serialize(e)}");
+                throw;
+            }
+        }
+    }
+}
